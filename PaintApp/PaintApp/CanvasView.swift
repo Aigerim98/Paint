dc1ea1aed311dcc37b6978a06d2a9b05dc736d3e
Aigerim Abdurakhmanova <aigerim.abdurakhmanova@IMAC-3-14.local>
2022-06-15 14:34:30 +0600

    problems with line

--- PaintApp/PaintApp/CanvasView.swift
+++ PaintApp/PaintApp/CanvasView.swift
@@ -7,16 +7,6 @@
 
 import UIKit
 
-struct TouchPointsAndColor {
-    var color: UIColor?
-    //var width: CGFloat?
-    var points: [CGPoint]?
-    
-    init(color: UIColor, points: [CGPoint]?){
-        self.color = color
-        self.points = points
-    }
-}
 
 enum DrawMode {
     case pen
@@ -28,64 +18,6 @@ enum DrawMode {
 
 class CanvasView: UIView {
     
-//    var lines = [TouchPointsAndColor]()
-//    var strokeColor: UIColor = .black
-//    var lastPoint = CGPoint.zero
-//    var drawLine: Bool = false
-//    override func draw(_ rect: CGRect) {
-//        super.draw(rect)
-//
-//        guard let context = UIGraphicsGetCurrentContext() else { return }
-//
-//        lines.forEach{ (line) in
-//            for (i, p) in (line.points?.enumerated())! {
-//                if i == 0 {
-//                    context.move(to: p)
-//                } else {
-//                    context.addLine(to: p)
-//                }
-//                context.setStrokeColor(line.color?.withAlphaComponent(1.0).cgColor ?? UIColor.black.cgColor)
-//                context.setLineWidth(5.0)
-//            }
-//            context.setLineCap(.round)
-//            context.strokePath()
-//        }
-//    }
-//
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        lines.append(TouchPointsAndColor(color: UIColor(), points: [CGPoint]()))
-//        guard let touch = touches.first else { return }
-//        lastPoint = touch.location(in: nil)
-//    }
-//
-//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        guard let touch = touches.first?.location(in: nil) else { return }
-//
-//        guard var lastPoint = lines.popLast() else {return}
-//        lastPoint.points?.append(touch)
-//        lastPoint.color = strokeColor
-//        lines.append(lastPoint)
-//        setNeedsDisplay()
-//    }
-//
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        if drawLine{
-//            drawLineFromPoint(start: lastPoint, toPoint: lastPoint, ofColor: strokeColor)
-//        }
-//    }
-//    func drawLineFromPoint(start : CGPoint, toPoint end:CGPoint, ofColor lineColor: UIColor) {
-//
-//        let path = UIBezierPath()
-//        path.move(to: start)
-//        path.addLine(to: end)
-//
-//        let shapeLayer = CAShapeLayer()
-//        shapeLayer.path = path.cgPath
-//        shapeLayer.strokeColor = strokeColor.cgColor
-//        shapeLayer.lineWidth = 1.0
-//
-//
-//    }
     var lineColor: UIColor = .black
     var lineWidth: CGFloat!
     var drawMode: DrawMode = .pen
@@ -126,7 +58,7 @@ class CanvasView: UIView {
         print(lastPoint)
         touchPoint = touch.location(in: self)
         //drawShapeLayer(startPoint: touchPoint, lastPoint: lastPoint)
-       // self.lastPoint = touchPoint
+        self.lastPoint = touchPoint
     }
     
     func drawShapeLayer(startPoint: CGPoint, lastPoint: CGPoint) {
